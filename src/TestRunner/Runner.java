9673a0c25d9dc704570a0601e23d23633c549f8b
satheesh swaminathan <32887710+deadlydexter@users.noreply.github.com>
2021-10-08 00:25:43 -0400

    fix runner file and upgrade guava dependency

--- src/TestRunner/Runner.java
+++ src/TestRunner/Runner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "Features", glue = { "StepDefinition" }, tags = "@UpdateProfile" , plugin = {
-		"html:target/cucumber-html-report" })
+		"html:target/cucumber-html-report/report.html" })
 public class Runner {
 
 }
